8f4578a8055c2c5a7c98f99aa87115bbbbb25638
Bryant <cabrera.bryant@gmail.com>
2019-04-03 13:44:16 -0700

    Creates Moms create route.

--- routers/moms.js
+++ routers/moms.js
@@ -23,4 +23,24 @@ router.get('/', async (req, res) => {
     }
 });
 
+//Create Route
+router.post('/', async (req, res) => {
+    console.log(req.body, 'hitting create Mom');
+
+    let hashedPassword = await bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+    req.body.password = hashedPassword;
+    try {
+        const createdMom = await Mom.create(req.body);
+        res.json({
+            status: 200,
+            data: 'Registration successful.',
+            createdMom: createdMom
+        });
+        
+    } catch (err) {
+        console.log(err);
+        res.send(err);
+    }
+});
+
 module.exports = router;
