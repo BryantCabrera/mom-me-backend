dae4cc666870927f3e6089048e36c53ea8e310b1
Bryant <cabrera.bryant@gmail.com>
2019-04-03 13:42:13 -0700

    Sets up moms.js Router.

---
+++ routers/moms.js
@@ -0,0 +1,13 @@
+/********** REQUIRES **********/
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+const Mom = require('../models/mom');
+
+/********** MIDDLEWARE **********/
+
+/********** ROUTES **********/
+//Index Route
+
+module.exports = router;
