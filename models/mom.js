acef962765792be35c38c19903b8638a601889b1
Bryant <cabrera.bryant@gmail.com>
2019-04-03 13:32:20 -0700

    Creates mom.js schema.

---
+++ models/mom.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const childSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    allergies: String,
+    bed_time: String
+});
+
+const momSchema = new mongoose.Schema({
+    name: { type: String, default: 'Parent' },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    img: { type: String, default: 'https://i.imgur.com/KbicDVh.jpg' },
+    children: [childSchema],
+    sex: String,
+    location: Number
+});
+
+const Mom = mongoose.model('Mom', momSchema);
+
+module.exports = Mom;
