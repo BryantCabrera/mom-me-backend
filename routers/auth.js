9e6a9027c18d28b2823866cca09f8c359e7717f2
Bryant <cabrera.bryant@gmail.com>
2019-04-03 14:18:37 -0700

    Sets up auth.js router.

---
+++ routers/auth.js
@@ -0,0 +1,13 @@
+/********** REQUIRES **********/
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+
+const Mom = require('../models/mom');
+
+/********** MIDDLEWARE **********/
+
+/********** ROUTES **********/
+// Log-In
+
+module.exports = router;
