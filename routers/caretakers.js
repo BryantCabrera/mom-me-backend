552f3b985cb825ce3a3f0b8fba660716de4f78f6
Bryant <cabrera.bryant@gmail.com>
2019-04-03 13:48:03 -0700

    Sets up caretakers.js router.

---
+++ routers/caretakers.js
@@ -0,0 +1,13 @@
+/********** REQUIRES **********/
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+
+const Caretaker = require('../models/caretaker');
+
+/********** MIDDLEWARE **********/
+
+/********** ROUTES **********/
+//Index Route
+
+module.exports = router;
